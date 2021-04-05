41a0f024cb55a97a234b9862d84ad14c6429783b
MinaNabil <minanabilsalib@gmail.com>
2021-04-05 12:26:11 +0200

    webpack-output-and-loaders

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,1 @@
-alert("i exist");
+(()=>{"use strict";console.log((function(e){console.log("::: Running checkForName :::",e),["Picard","Janeway","Kirk","Archer","Georgiou"].includes(e)&&alert("Welcome, Captain!")})),alert("i exist")})();
